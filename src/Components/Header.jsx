4091b561d9a9b764e5e516084bf7847e99f4df9e
akashsingh_bhandari <asbhandari14@gmail.com>
2024-04-13 07:02:09 +0530

    Little bit changes in the Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Header=()=>{
     return(
         <>
-            <div className="section w-full h-[25vh] flex justify-center items-center my-6">
+            <div className="section w-full h-[25vh] flex justify-center items-center my-8">
                 <div className="container w-[80%] h-full flex flex-col justify-center items-center gap-4">
                     <h1 className="text-lg">Search Your Favourite Movie</h1>
                     <input className=" py-1.5 px-3 rounded-xl w-72 border-2 border-black" type="text" placeholder="Search" />
